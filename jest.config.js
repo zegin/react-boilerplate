7ec82c041fb52aba7085d8e1d261e0ddd9574a14
Gilian GONNORD <gi-gonnord@DOMSR.LOCAL>
2018-11-09 17:08:02 +0100

    Move jest config and update readme

--- jest.config.js
+++ jest.config.js
@@ -3,5 +3,8 @@ module.exports = {
     setupFiles: [
         '<rootDir>/.jest/register-context.js',
         '<rootDir>/.jest/enzyme-setup.js'
-    ]
+    ],
+    moduleNameMapper: {
+      "\\.(gif|ttf|eot|svg)$": "<rootDir>/test/client/__mocks__/fileMock.js"
+    }
 };
